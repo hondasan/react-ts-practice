0d92dbf44bc9605a26f9cb35288f269f1c53d9d7
hondasan <86158971+hondasan@users.noreply.github.com>
2022-05-27 23:28:20 +0900

    オプショナルチェインニング

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,14 @@ import { useState } from "react";
 import "./styles.css";
 import { Todo } from "./Todo";
 import { Text } from "./Text";
+import { UserProfile } from "./UserProfile";
 import { TodoType } from "./typs/todo";
+import { User } from "./typs/user";
+
+const user: User = {
+  name: "兄貴"
+  // hobbies: ["映画", "ゲーム"]
+};
 
 export default function App() {
   const [todos, setTodos] = useState<Array<TodoType>>([]);
@@ -16,6 +23,7 @@ export default function App() {
   };
   return (
     <div className="App">
+      <UserProfile user={user} />
       <Text color="red" fontSize="18px" />
       <button onClick={onClickFetchData}>データ取得</button>
       {todos.map((todo) => (
